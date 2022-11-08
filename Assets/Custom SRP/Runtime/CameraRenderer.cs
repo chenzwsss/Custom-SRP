5d72f296e33a33d28247ad760adfbee9d1018606
chenzwsss <746869297@qq.com>
2022-11-08 16:13:05 +0800

    1.Custom Render Pipeline
    1.Custom Render Pipeline

---
+++ Assets/Custom SRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    const string bufferName = "Render Camera Czw";
+
+    // use context.DrawSkybox to draw the skybox
+    // via a separate command buffer to draw the other geometry
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    CullingResults cullingResults;
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        PrepareBuffer();
+        // draw UI in scene window
+        PrepareForSceneWindow();
+
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+
+        DrawVisibleGeometry();
+
+        DrawUnsupportedShaders();
+
+        // draw gizmos in scene window
+        DrawGizmos();
+
+        // submit the queued work for execution
+        Submit();
+    }
+
+    void Setup()
+    {
+        // apply camera's properties to the context
+        context.SetupCameraProperties(camera);
+
+        CameraClearFlags flags = camera.clearFlags;
+
+        // clear camera's render target
+        buffer.ClearRenderTarget(flags <= CameraClearFlags.Depth, flags == CameraClearFlags.Color,
+            flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.clear);
+
+        // begin inject profiler event (show up in profiler & frame debugger)
+        buffer.BeginSample(SampleName);
+
+        ExecuteBuffer();
+    }
+
+    void DrawVisibleGeometry()
+    {
+        // draw opaque
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        // draw skybox
+        context.DrawSkybox(camera);
+
+        // draw transparent
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    void Submit()
+    {
+        // end inject profiler event (show up in profiler & frame debugger)
+        buffer.EndSample(SampleName);
+
+        ExecuteBuffer();
+
+        context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        // execute the buffer
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+
+        return false;
+    }
+}
